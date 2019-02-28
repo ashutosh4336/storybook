7c194346f47edef8d8540d64f4af1da682a695d4
Ashutosh Panda <ashu.juga99@gmail.com>
2019-02-28 22:21:21 +0530

    first push to Heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
