026776b900b51dc8a1a8a5fd2e92e69f303d37d4
Ashutosh Panda <ashu.juga99@gmail.com>
2019-02-28 23:26:26 +0530

    Intial Redirect to google

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+//Use Routes
+app.use('/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
